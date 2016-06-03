e3b7f0c3c8386e33befc78a9a69c4fff62ebe5c1
Chris Thomas <chris.thomas@antimatter-studios.com>
2016-06-03 10:51:04 +0200

    update both of these definitions to add missing event handlers

--- src/FormControl.d.ts
+++ src/FormControl.d.ts
@@ -8,7 +8,10 @@ export interface FormControlProps extends React.Props<FormControlClass> {
   type: string;
   id?: string;
   name?: string;
+  value?: string | number;
   placeholder?: string;
+  onChange: Function;
+  onKeyPress: Function;
 }
 export interface FormControl extends React.ReactElement<FormControlProps> {}
 export interface FormControlClass extends React.ComponentClass<FormControlProps> {
